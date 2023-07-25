623a495d5428ce72b0b06955b188447043f0ef89
Ekta549 <133002329+Ekta549@users.noreply.github.com>
2023-07-25 15:05:16 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, avoid_print
-
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import 'package:mentalhealthapp/main.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,138 +9,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Sample list of items for the ListView.builder
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[800],
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(25.0),
-            child: Column(children: [
-              AppBar(
-                toolbarHeight: 45,
-                title: Text('Welcome'),
-                centerTitle: true,
-                leading: Icon(Icons.menu),
-                actions: [
-                  IconButton(
-              
-                      icon: Icon(Icons.search),
-                      onPressed: () {
-                        print("Search pressed");
-                      })
-                ],
-                //greetings row
+      backgroundColor: Colors.blue[800],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(children: [
+            AppBar(
+              toolbarHeight: 45,
+              title: const Text('Welcome'),
+              centerTitle: true,
+              leading: const Icon(Icons.menu),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    // ignore: avoid_print
+                    print("Search pressed");
+                  },
+                )
+              ],
+            ),
+
+            // Rest of the UI elements
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            // Insert ListView.builder here
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      // Handle item tap here if needed
+                    },
+                  );
+                },
               ),
-
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Hi Jared!
-                    Column(
-                      children: [
-                        Text(
-                          'Hi, Jared!',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ), //SizeBox
-
-                        Text(
-                          '23 Jan, 2023',
-                          style: TextStyle(color: Colors.blue[100]),
-                        ),
-                      ],
-                    ),
-                    //Notification
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue[600],
-                        borderRadius: BorderRadius.circular(12),
-                      ), //BoxDecoration
-                      padding: EdgeInsets.all(12),
-                      child: Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-
-              SizedBox(
-                height: 25,
-              ),
-
-              //search bar
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue[600],
-                  borderRadius: BorderRadius.circular(12),
-                ), //BoxDecoration
-                padding: EdgeInsets.all(12),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      'Search',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(
-                height: 25,
-              ),
-
-              //how do you feel?
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'How do you feel?',
-                  ),
-                  Icon(
-                    Icons.more_horiz,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-
-              //4 different faces
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //bad
-
-                  //fine
-
-                  //well
-
-                  //excellent
-                ],
-              ),
-            ]),
-          ),
-        ));
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
